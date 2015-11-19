27aecd970f08f913fada201690cd682b44bc6dd2
adgalad <carlosspaggiari@carloss-macbook-pro.local>
2015-11-19 17:15:27 -0430

    Initial Commit

---
+++ GameEngineV2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameEngineV2
+//
+//  Created by Carlos Spaggiari Roa on 11/19/15.
+//  Copyright © 2015 ARSC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
